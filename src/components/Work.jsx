f8cc0954cc13bc8271394b5f3b3000436338e46c
Javier Cavalli <javier.cavalli@gmail.com>
2022-05-13 17:32:28 -0300

    updating work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,7 +3,7 @@ import cryptosite from "../assets/cryptosite.png";
 import darktheme from "../assets/darktheme.png";
 import expensetracker from "../assets/expensetracker.png";
 import ecommerce from "../assets/ecommerce.png";
-import moviesapp from "../assets/moviesapp.png";
+import watchflix from "../assets/watchflix.png";
 import dataapp from "../assets/dataapp.jpg";
 
 const Work = () => {
@@ -52,19 +52,19 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${darktheme})` }}
+            style={{ backgroundImage: `url(${watchflix})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                Dark Theme UI
+                Movie/Api App
               </span>
               <div className="pt-8 text-center">
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://spontaneous-travesseiro-fade16.netlify.app/"
+                  href="https://watchflix-main.vercel.app/"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
@@ -73,7 +73,7 @@ const Work = () => {
                 <a
                   target="_blank"
                   rel="noopener noreferrer"
-                  href="https://github.com/JavierCavalli/dark-theme-main"
+                  href="https://github.com/JavierCavalli/watchflix-main"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
@@ -83,37 +83,6 @@ const Work = () => {
             </div>
           </div>
           {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${expensetracker})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Expense Tracker
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://aquamarine-nasturtium-4bf446.netlify.app/"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/JavierCavalli/expense-tracker-react-main"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div
             style={{ backgroundImage: `url(${ecommerce})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -145,38 +114,6 @@ const Work = () => {
               </div>
             </div>
           </div>
-          {/* Grid Item */}
-          <div
-            style={{ backgroundImage: `url(${moviesapp})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Movie/Api App
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://fastidious-jelly-05b421.netlify.app/"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://github.com/JavierCavalli/react-movie-main"
-                >
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div
             style={{ backgroundImage: `url(${cryptosite})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
@@ -200,6 +137,69 @@ const Work = () => {
                   target="_blank"
                   rel="noopener noreferrer"
                   href="https://github.com/JavierCavalli/Crypto-landingpage-main"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${darktheme})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Dark Theme UI
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://spontaneous-travesseiro-fade16.netlify.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/JavierCavalli/dark-theme-main"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${expensetracker})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Expense Tracker
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://aquamarine-nasturtium-4bf446.netlify.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/JavierCavalli/expense-tracker-react-main"
                 >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
